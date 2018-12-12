15bd71c510ed029882bcd38be994d5979e636db0
reneecosmic <renee.hickman@outlook.com>
2018-12-12 08:32:21 -0600

    js started

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+var wordOptions =           // Word list
+    [
+        "STEVEN STAMKOS",
+        "NIKITA KUCHEROV",
+        "ANTON STRALMAN",
+        "RYAN CALLAHAN",
+        "ANDRE VASILEVSKIY",
+        "VICTOR HEDMAN",
+        "BRAYDEN POINT",
+        "TYLER JOHNSON",
+    ];
+
+
+    // max number or guesses
+    TotalGuesses = 
+
+    // letters the player has guessed
+    var lettersGuessed = 
