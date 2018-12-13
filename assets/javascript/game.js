070089f249ee09faf371ae24260259aaf7e296c5
reneecosmic <renee.hickman@outlook.com>
2018-12-13 07:45:33 -0600

    12/12

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,40 @@
-var wordOptions =           // Word list
-    [
-        "STEVEN STAMKOS",
-        "NIKITA KUCHEROV",
-        "ANTON STRALMAN",
-        "RYAN CALLAHAN",
-        "ANDRE VASILEVSKIY",
-        "VICTOR HEDMAN",
-        "BRAYDEN POINT",
-        "TYLER JOHNSON",
-    ];
+// Word list
+var wordOptions = [
+    "STEVEN STAMKOS",
+    "NIKITA KUCHEROV",
+    "ANTON STRALMAN",
+    "RYAN CALLAHAN",
+    "ANDRE VASILEVSKIY",
+    "VICTOR HEDMAN",
+    "BRAYDEN POINT",
+    "TYLER JOHNSON",
+];
 
 
-    // max number or guesses
-    TotalGuesses = 
 
-    // letters the player has guessed
-    var lettersGuessed = 
+// Create variables that hold references to the places in the HTML where we want to display things.
+
+// max number or guesses
+TotalGuesses = 10;
+
+// letters the player has guessed
+var lettersGuessed = [];
+
+// letters the player has guessed
+var guessesRemaining = [];
+
+// active word in wordOptions array
+var activeWord;
+
+// Total number of rounds user has won
+var winsTotal = 0;
+
+
+// This function is run whenever the user presses a key
+document.onkeyup = function (event) {
+
+// Determines which key was pressed.
+var userGuess = event.key;
+
+
+};
