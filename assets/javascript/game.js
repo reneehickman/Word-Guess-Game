d45738272398c06e134d28bee82a3e915c0e5a2e
Renee Hickman <renee.hickman@outlook.com>
2019-02-07 16:41:27 -0600

    html/css/js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,152 +1,139 @@
-window.onload = function () {
+// window.onload = function () {
 
-    // Word list
-    // var wordOptions = [
-    //         "stamkos",
-    //         "kucherov",
-    //         "stralman",
-    //         "callahan",
-    //         "vasilevskiy",
-    //         "hedman",
-    //         "point",
-    //         "johnson",
-    //     ];
+var players = [{
+    name: "stamkos",
+    image: "stamkos"
+},
 
-
-
-    var players = [{
-        name: "stamkos",
-        image: "stamkos"
-    },
-    
-    {
-        name: "kucherov",
-        image: "kucherov"
-    },
-    {
-        name: "stralman",
-        image: "stralman"
-    },
-    {
-        name: "callahan",
-        image: "callahan"
-    },
-    {
-        name: "vasilevskiy",
-        image: "vasilevskiy"
-    },
-    {
-        name: "hedman",
-        image: "hedman"
-    },
-    {
-        name: "point",
-        image: "point"
-    },
-    {
-        name: "johnson",
-        image: "johnson"
-    }
+{
+    name: "kucherov",
+    image: "kucherov"
+},
+{
+    name: "stralman",
+    image: "stralman"
+},
+{
+    name: "callahan",
+    image: "callahan"
+},
+{
+    name: "vasilevskiy",
+    image: "vasilevskiy"
+},
+{
+    name: "hedman",
+    image: "hedman"
+},
+{
+    name: "point",
+    image: "point"
+},
+{
+    name: "johnson",
+    image: "johnson"
+}
 ]
 
 
- // Create variables that hold references to the places in the HTML where we want to display things.
-    var winsTotalText = document.getElementById("winsTotal");
-    var activeWordText = document.getElementById("activeWord");
-    var guessesRemainingText = document.getElementById("guessesRemaining");
-    var lettersGuessedText = document.getElementById("lettersGuessed");
-    var hintElement = document.getElementById("");
-    var tryAgainText = document.getElementById("pressKeyTryAgain");
-    var directionsTextElement = document.getElementById("directionsText");
+// hintElement.innerHTML = "<img src='"+wordOptions["point"]+"'>";
 
-    // hintElement.innerHTML = "<img src='"+wordOptions["point"]+"'>";
+// Create variables that hold references to the places in the HTML where we want to display things.
+var winsTotalText = document.getElementById("winsTotal");
+var activeWordText = document.getElementById("activeWord");
+var guessesRemainingText = document.getElementById("guessesRemaining");
+var lettersGuessedText = document.getElementById("lettersGuessed");
+var hintElement = document.getElementById("hangmanImg");
+var tryAgainText = document.getElementById("tryAgain");
+var youLoseText = document.getElementById("youLose");
+var youWinText = document.getElementById("youWin");
+var directionsTextElement = document.getElementById("directionsText");
 
-    // max number of guesses
-    var TotalGuesses = 10;
+// max number of guesses
+var TotalGuesses = 10;
 
-    // Each letter the player guesses/userGuess is pushed into this array
-    var lettersGuessed = [];
+// Each letter the player guesses/userGuess is pushed into this array
+var lettersGuessed = [];
 
-    // number of guesses remaining
-    var guessesRemaining = [];
+// number of guesses remainingd
+var guessesRemaining = [];
 
-    // active word in wordOptions array
-    var activeWord = "";
+// Array that holds the letters of the activeWord
+var emptyWordArray = [];
 
-    // Create variables that hold references to the places in the HTML where we want to display things.
-    var winsTotalText = document.getElementById("winsTotal");
-    var activeWordText = document.getElementById("activeWord");
-    var guessesRemainingText = document.getElementById("guessesRemaining");
-    var lettersGuessedText = document.getElementById("lettersGuessed");
-    var hintElement = document.getElementById("");
-    var tryAgainText = document.getElementById("pressKeyTryAgain");
-    var directionsTextElement = document.getElementById("directionsText");
+// active word in wordOptions array
+var activeWordIndex = [Math.floor(Math.random() * players.length)];
 
+var activeWord = players[activeWordIndex].name;
 
-    // max number of guesses
-    var TotalGuesses = 10;
+//Array of all acceptable key strokes (ie only lowercase letters)
+// var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    // Each letter the player guesses/userGuess is pushed into this array
-    var lettersGuessed = [];
+var gamePlay = {
 
-    // number of guesses remainingd
-    var guessesRemaining = [];
-
-    // active word in wordOptions array
-    var activeWord = players[Math.floor(Math.random() * players.length)];
-
-    //Array of all acceptable key strokes (ie only lowercase letters)
-    var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    startGame: function(){
+        gamePlay.createEmptySpaces();
+        gamePlay.displayHint();
+        youLoseText.style.display = 'none';
+        youWinText.style.display = 'none';
+        tryAgainText.style.display = 'none';
+    },
 
 
-
-    function createEmptySpaces(){
-    // Array that holds the letters of the activeWord
-    var emptyWordArray = [];
-    for (var i = 0; i < activeWord.length; i++) {
-        emptyWordArray[i] = "_ ";
-        activeWordText.textContent = emptyWordArray.join(' ');
-    }
-
-    console.log(emptyWordArray);
-
-    console.log(activeWord);
-
-}
-
-
-
-
-    // Total number of rounds user has won
-    var winsTotal = 0;
-
-
-
-    // This function is run whenever the user presses a key
-    document.onkeyup = function (event) {
-
-        // Determines which key was pressed.
-        var userGuess = event.key;
-
-        // Hide the directions
-        directionsTextElement.textContent = "";
-
-
-        lettersGuessedText.textContent = userGuess;
-
-        for (var j = 0; j < activeWord.length; j++) {
-            if (activeWord[j] === userGuess) {
-                emptyWordArray[j] = userGuess;
-                activeWordText.textContent = emptyWordArray.join(' ');
-                //lettersRemaining--;
-            }
-
+    createEmptySpaces: function () {
+        for (var i = 0; i < activeWord.length; i++) {
+            emptyWordArray[i] = "_ ";
+            activeWordText.textContent = emptyWordArray.join(' ');
         }
 
         console.log(emptyWordArray);
 
+        console.log(activeWord);
+
+    },
 
 
-    }
+    displayHint: function () {
+        // for (var i = 0; i < players.length; i++) {
+            hintElement.innerHTML = '<img src="assets/images/' + players[activeWordIndex].image + '.jpg">';
+        // }
+    },
 
-};
+
+}
+
+gamePlay.startGame();
+
+// Total number of rounds user has won
+var winsTotal = 0;
+
+// This function is run whenever the user presses a key
+document.onkeyup = function (event) {
+
+    // gamePlay.startGame();
+    // gamePlay.createEmptySpaces();
+
+    // Determines which key was pressed.
+    var userGuess = event.key;
+
+    // Hide the directions
+    directionsTextElement.textContent = "";
+
+
+    lettersGuessedText.textContent = userGuess;
+
+    // for (var i = 0; i < activeWord.length; i++) {
+    //     if (activeWord[i] === userGuess) {
+    //         emptyWordArray[i] = userGuess;
+    //         activeWordText.textContent = emptyWordArray.join(' ');
+    //         //lettersRemaining--;
+    //     }
+
+    // }
+
+    // console.log(emptyWordArray);
+
+
+
+}
+
